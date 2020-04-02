f349b8bd8280eb8d0c961b7c9a53959624a6029d
andrii <werdon2008@gmail.com>
2020-04-02 19:33:52 +0300

    async-examples
    apply formatting

--- generators/generators.js
+++ generators/generators.js
@@ -1,27 +1,28 @@
-function* squareGenerator(){
-    let b = 1;
-    while (true){
-        b+=1;
-        yield b * b;
-    }
+function* squareGenerator() {
+  let b = 1;
+  while (true) {
+    b+=1;
+    yield b * b;
+  }
 }
 
 // Continuously cuts a number in half.
-function* halfGenerator(e = 64){
-    let c = e;
-    while (true) {
-        c /= 2;
-        if (c > 1) {
-            yield c;
-        } else {
-            return c;
-        }
+function* halfGenerator(e = 64) {
+  let c = e;
+  while (true) {
+    c /= 2;
+    if (c > 1) {
+      yield c;
+    } else {
+      return c;
     }
+  }
 }
 
 function generatorCoroutine(Generator,...args) {
-    let gen = Generator(...args);
-    setInterval(()=>{
-        console.log(gen.next());
-    }, 250);
+  const gen = Generator(...args);
+
+  setInterval(()=>{
+    console.log(gen.next());
+  }, 250);
 }
