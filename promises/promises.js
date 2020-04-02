80b0dd4e339ee1d98232ae901f5ea112ef7e8f95
andrii <werdon2008@gmail.com>
2020-04-02 19:27:21 +0300

    async-examples: add changes
    Apply changes

--- promises/promises.js
+++ promises/promises.js
@@ -1,26 +1,26 @@
-function promiseFunction(a,b){
-    let c = a + b;
-    return new Promise(
-        function(resolve,reject){
-            setTimeout(resolve,c,250);
-        }
-    )
+function promiseFunction(a,b) {
+  const c = a + b;
+
+  return new Promise(
+    function(resolve,reject) {
+      setTimeout(resolve, 250, c);
+    }
+  )
 }
 
 // Functions like the above promise but with a clause allowing for the "catch" functionality to be used
 function promiseFunctionWithFailure(a,b){
-    let c = a * b;
-    let success = Math.random() > 0.5;
-    return new Promise(
-        function(resolve,reject){
-            success ? resolve(c) : reject("ERROR!")
-        }
-    )
+  const c = a * b;
+  const success = Math.random() > 0.5;
+
+  return new Promise(function(resolve,reject){
+    success ? resolve(c) : reject("ERROR!")
+  })
 }
 
 promiseFunction(2,3)
-    .then(c=>console.log(c));
+    .then(console.log);
 
 promiseFunctionWithFailure(2,4)
-    .then(c=>console.log("Success!",c))
-    .catch(c=>console.error(c));
+    .then(c => console.log("Success!",c))
+    .catch(console.error);
